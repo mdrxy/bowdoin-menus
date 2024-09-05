22e206b19c193ef3fe9c9d384f021ecd5512ad96
WBOR 91.1 FM <wbor@bowdoin.edu>
2024-09-05 10:43:13 -0400

    Use .env, handle "None" objects, general cleanup

--- driver.py
+++ driver.py
@@ -21,19 +21,17 @@ class Meals():
     BRUNCH = "brunch"
     LUNCH = "lunch"
     DINNER = "dinner"
-    
+
     def getUpcomingMeal(location):
         """
         The next upcoming meal is set after the current meal expires.
-        
         During a meal period, it is still "upcoming".
-        
         Only handles full hours, so 12:30 p.m. is rounded up to 1 p.m.
         """
 
         currentHour = datetime.datetime.now().time().hour
         currentDay = datetime.datetime.now().strftime("%a").lower()
-        
+
         if location == Location.MOULTON:
             # Monday–Friday
             if currentDay != "sat" and currentDay != "sun":
@@ -52,7 +50,7 @@ class Meals():
 
             # Saturday–Sunday
             if currentDay == "sat" or currentDay == "sun":
-                # Breakfast: 8:00 a.m. to 11:00 a.m.            
+                # Breakfast: 8:00 a.m. to 11:00 a.m.
                 if 0 <= currentHour < 11 or 19 <= currentHour < 24:
                     if currentDay == "sun" and 19 <= currentHour < 24:
                         return Meals.BREAKFAST
@@ -60,35 +58,31 @@ class Meals():
                 # Brunch: 11:00 a.m. to 12:30 p.m.
                 if 11 <= currentHour < 13:
                     return Meals.LUNCH
-                # Dinner: 5:00 p.m. to 7:00 p.m.    
+                # Dinner: 5:00 p.m. to 7:00 p.m.
                 if 13 <= currentHour < 19:
                     return Meals.DINNER
-            
+
         if location == Location.THORNE:
             # Monday–Friday
             if currentDay != "sat" and currentDay != "sun":
-                # print("Weekday")
+
                 # Breakfast: 8:00 a.m. to 10:00 a.m.
                 if 0 <= currentHour < 10 or 20 <= currentHour < 24:
                     if (currentDay == "fri" and 20 <= currentHour < 24):
-                        # print("Brunch")
                         return Meals.BRUNCH
                     else:
-                        print("Breakfast")
                         return Meals.BREAKFAST
+
                 # Lunch: 11:30 a.m. to 2:00 p.m.
                 if 10 <= currentHour < 14:
-                    # print("Lunch")
                     return Meals.LUNCH
                 # Dinner: 5:00 p.m. to 8:00 p.m.
                 if 14 <= currentHour < 20:
-                    # print("Dinner")
                     return Meals.DINNER
 
             # Saturday–Sunday
             if currentDay == "sat" or currentDay == "sun":
-                # print("Sat or Sun")
-                
+
                 # Brunch: 11:00 a.m. to 1:30 p.m.
                 if 0 <= currentHour < 14 or 20 <= currentHour < 24:
                     if currentDay == "sun" and 20 <= currentHour < 24:
@@ -97,30 +91,30 @@ class Meals():
                 # Dinner:  5:00 p.m. to 7:30 p.m.
                 if 14 <= currentHour < 20:
                     return Meals.DINNER
-    
+
 def buildRequest(location):
     currentDate = datetime.datetime.now().strftime("%Y%m%d")
     locationUnit = location
-    
+
     requestData = {
         'unit': {locationUnit},
         'date': {currentDate},
         'meal': {Meals.getUpcomingMeal(location)}
     }
-    
+
     return requestData
 
 def request(location):
     response = requests.post(menuAPI, data=buildRequest(location))
-    
+
     if response.status_code == 200:
         return response.content
     else:
         print("Error:", response.status_code)
-    
+
 def parseResponse(requestContent):
     root = ET.fromstring(requestContent)
-    
+
     courseValues = []
     itemNames = []
 
@@ -129,13 +123,13 @@ def parseResponse(requestContent):
         # Extract 'course' and 'formal_name' values from each 'record' element
         course = record.find('course').text
         webLongName = record.find('webLongName').text
-        
+
         # Append the values to the respective lists
         courseValues.append(course)
         itemNames.append(webLongName)
-    
+
     menu = {key: [] for key in set(courseValues)}
-    
+
     i = 0
     for item in itemNames:
         if item: # Sometimes a NoneType item would be returned
@@ -143,67 +137,53 @@ def parseResponse(requestContent):
             item = re.sub(r'\s+', ' ', item)
         menu[courseValues[i]].append(item)
         i += 1
-        
+
     custom_order = ['Main Course', 'Desserts']
     sorted_menu = {key: menu[key] for key in custom_order if key in menu}
     sorted_menu.update({key: menu[key] for key in menu if key not in sorted_menu})    
-    
+
     return sorted_menu
 
 def stringify(location, menu):
     meal = Meals.getUpcomingMeal(location)
-    
+
     outputString = ""
-    if location is Location.MOULTON:    
+    if location is Location.MOULTON:
         outputString += f"Moulton Union {meal.capitalize()}:"
-    if location is Location.THORNE:    
+    if location is Location.THORNE:
         outputString += f"Thorne {meal.capitalize()}:"
     outputString += '\n\n'
 
     for category, items in menu.items():
         outputString += f"{category}:\n"
         for item in items:
-            outputString += f"- {item}\n"
+            if item != "None":
+                outputString += f"- {item}\n"
         outputString += "\n"
-        
+
     return outputString
 
 def sendMessage(text):
-    jail_date = datetime.datetime(year=2023, month=3, day=29)
-    current_date = datetime.datetime.now()
-    days_difference = (current_date - jail_date).days
-    
-    evan = ""
-    evan += text
-    evan += f"Evan Gershkovich:\n- {days_difference} days"
-    
     data = {
-        'text': evan,
+        'text': text,
         'bot_id': botID
     }
-    
-    headers = {'Content-Type': 'application/json'}
-    
-    response = requests.post(groupmeAPI, data=json.dumps(data), headers=headers)
 
-    if response.status_code == 200 or \
-        response.status_code == 201 or \
-            response.status_code == 202:
-        print("Message posted successfully.")
-    else:
-        print("Error:", response.status_code)
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.post(groupmeAPI, data=json.dumps(data), headers=headers)
 
 if __name__ == "__main__":
     thorne = request(Location.THORNE)
     thorneMenu = parseResponse(thorne)
-    
+
     moulton = request(Location.MOULTON)
     moultonMenu = parseResponse(moulton)
 
     thorneText = stringify(Location.THORNE, thorneMenu)
     if len(thorneText) < 1000:
-        sendMessage(thorneText)    
-    
+        sendMessage(thorneText)
+
     moultonText = stringify(Location.MOULTON, moultonMenu)
     if len(moultonText) < 1000:
         sendMessage(moultonText)
