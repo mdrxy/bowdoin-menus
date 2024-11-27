519ebfc62e769806796ac7dcb79fbd47d2561a57
Mason Daugherty <mdaugher@bowdoin.edu>
2024-11-27 00:44:51 -0600

    Pylint

--- driver.py
+++ driver.py
@@ -1,115 +1,130 @@
+"""
+Module to retrieve and send the current menu for Moulton Union and Thorne Hall.
+"""
 import os
-from dotenv import load_dotenv
-import requests
 import datetime
-import xml.etree.ElementTree as ET
 import json
 import re
+import xml.etree.ElementTree as ET
+from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 botID = os.getenv("BOT_ID")
-menuAPI = "https://apps.bowdoin.edu/orestes/api.jsp"
-groupmeAPI = "https://api.groupme.com/v3/bots/post"
+MENU_API = "https://apps.bowdoin.edu/orestes/api.jsp"
+GROUPME_API = "https://api.groupme.com/v3/bots/post"
 
 
 class Location:
+    """
+    Represents the location of the menu.
+    """
     MOULTON = 48
     THORNE = 49
 
 
 class Meals:
+    """
+    Represents the meal period.
+    """
     BREAKFAST = "breakfast"
     BRUNCH = "brunch"
     LUNCH = "lunch"
     DINNER = "dinner"
 
-    def getUpcomingMeal(location):
+    def get_upcoming_meal(self, location):
         """
         The next upcoming meal is set after the current meal expires.
         During a meal period, it is still "upcoming".
         Only handles full hours, so 12:30 p.m. is rounded up to 1 p.m.
         """
 
-        currentHour = datetime.datetime.now().time().hour
-        currentDay = datetime.datetime.now().strftime("%a").lower()
+        current_hour = datetime.datetime.now().time().hour
+        current_day = datetime.datetime.now().strftime("%a").lower()
 
         if location == Location.MOULTON:
             # Monday–Friday
-            if currentDay != "sat" and currentDay != "sun":
+            if current_day != "sat" and current_day != "sun":
                 # Breakfast: 7:00 a.m. to 10:00 a.m.
-                if 0 <= currentHour < 10 or 19 <= currentHour < 24:
-                    if currentDay == "fri" and 19 <= currentHour < 24:
+                if 0 <= current_hour < 10 or 19 <= current_hour < 24:
+                    if current_day == "fri" and 19 <= current_hour < 24:
                         return Meals.BRUNCH
                     else:
                         return Meals.BREAKFAST
                 # Lunch: 11:00 a.m. to 2:00 p.m.
-                if 10 <= currentHour < 14:
+                if 10 <= current_hour < 14:
                     return Meals.LUNCH
                 # Dinner: 5:00 p.m. to 7:00 p.m.
-                if 14 <= currentHour < 19:
+                if 14 <= current_hour < 19:
                     return Meals.DINNER
 
             # Saturday–Sunday
-            if currentDay == "sat" or currentDay == "sun":
+            if current_day == "sat" or current_day == "sun":
                 # Breakfast: 8:00 a.m. to 11:00 a.m.
-                if 0 <= currentHour < 11 or 19 <= currentHour < 24:
-                    if currentDay == "sun" and 19 <= currentHour < 24:
+                if 0 <= current_hour < 11 or 19 <= current_hour < 24:
+                    if current_day == "sun" and 19 <= current_hour < 24:
                         return Meals.BREAKFAST
                     return Meals.BRUNCH
                 # Brunch: 11:00 a.m. to 12:30 p.m.
-                if 11 <= currentHour < 13:
+                if 11 <= current_hour < 13:
                     return Meals.LUNCH
                 # Dinner: 5:00 p.m. to 7:00 p.m.
-                if 13 <= currentHour < 19:
+                if 13 <= current_hour < 19:
                     return Meals.DINNER
 
         if location == Location.THORNE:
             # Monday–Friday
-            if currentDay != "sat" and currentDay != "sun":
+            if current_day != "sat" and current_day != "sun":
 
                 # Breakfast: 8:00 a.m. to 10:00 a.m.
-                if 0 <= currentHour < 10 or 20 <= currentHour < 24:
-                    if currentDay == "fri" and 20 <= currentHour < 24:
+                if 0 <= current_hour < 10 or 20 <= current_hour < 24:
+                    if current_day == "fri" and 20 <= current_hour < 24:
                         return Meals.BRUNCH
                     else:
                         return Meals.BREAKFAST
 
                 # Lunch: 11:30 a.m. to 2:00 p.m.
-                if 10 <= currentHour < 14:
+                if 10 <= current_hour < 14:
                     return Meals.LUNCH
                 # Dinner: 5:00 p.m. to 8:00 p.m.
-                if 14 <= currentHour < 20:
+                if 14 <= current_hour < 20:
                     return Meals.DINNER
 
             # Saturday–Sunday
-            if currentDay == "sat" or currentDay == "sun":
+            if current_day == "sat" or current_day == "sun":
 
                 # Brunch: 11:00 a.m. to 1:30 p.m.
-                if 0 <= currentHour < 14 or 20 <= currentHour < 24:
-                    if currentDay == "sun" and 20 <= currentHour < 24:
+                if 0 <= current_hour < 14 or 20 <= current_hour < 24:
+                    if current_day == "sun" and 20 <= current_hour < 24:
                         return Meals.BREAKFAST
                     return Meals.BRUNCH
                 # Dinner:  5:00 p.m. to 7:30 p.m.
-                if 14 <= currentHour < 20:
+                if 14 <= current_hour < 20:
                     return Meals.DINNER
 
 
-def buildRequest(location):
-    currentDate = datetime.datetime.now().strftime("%Y%m%d")
-    locationUnit = location
+def build_request(location):
+    """
+    Builds the request data to be sent to the menu API.
+    """
+    current_date = datetime.datetime.now().strftime("%Y%m%d")
+    location_unit = location
 
-    requestData = {
-        "unit": {locationUnit},
-        "date": {currentDate},
-        "meal": {Meals.getUpcomingMeal(location)},
+    request_data = {
+        "unit": {location_unit},
+        "date": {current_date},
+        "meal": {Meals().get_upcoming_meal(location)},
     }
 
-    return requestData
+    return request_data
 
 
 def request(location):
-    response = requests.post(menuAPI, data=buildRequest(location))
+    """
+    Makes a POST request to the menu API.
+    """
+    response = requests.post(MENU_API, data=build_request(location), timeout=10)
 
     if response.status_code == 200:
         return response.content
@@ -117,30 +132,33 @@ def request(location):
         print("Error:", response.status_code)
 
 
-def parseResponse(requestContent):
-    root = ET.fromstring(requestContent)
+def parse_response(request_content):
+    """
+    Parses the XML response from the menu API.
+    """
+    root = ET.fromstring(request_content)
 
-    courseValues = []
-    itemNames = []
+    course_values = []
+    item_names = []
 
     # Iterate over each 'record' element in the XML
     for record in root.findall(".//record"):
         # Extract 'course' and 'formal_name' values from each 'record' element
         course = record.find("course").text
-        webLongName = record.find("webLongName").text
+        web_long_name = record.find("web_long_name").text
 
         # Append the values to the respective lists
-        courseValues.append(course)
-        itemNames.append(webLongName)
+        course_values.append(course)
+        item_names.append(web_long_name)
 
-    menu = {key: [] for key in set(courseValues)}
+    menu = {key: [] for key in set(course_values)}
 
     i = 0
-    for item in itemNames:
+    for item in item_names:
         if item:  # Sometimes a NoneType item would be returned
             # Remove consecutive spaces from Strings
             item = re.sub(r"\s+", " ", item)
-        menu[courseValues[i]].append(item)
+        menu[course_values[i]].append(item)
         i += 1
 
     custom_order = ["Main Course", "Desserts"]
@@ -151,49 +169,64 @@ def parseResponse(requestContent):
 
 
 def stringify(location, menu):
-    meal = Meals.getUpcomingMeal(location)
+    """
+    Converts the menu dictionary into a formatted string.
+    
+    Parameters:
+    - location: The location of the menu (Moulton or Thorne).
+    - menu: The menu dictionary to be converted.
+    """
+    meal = Meals().get_upcoming_meal(location)
 
-    # Note that this is the timestamp from when the script is called, not the date the menu is being served
+    # Note that this is the timestamp from when the script is called
+    # (not the date the menu is being served)
     timestamp = datetime.datetime.now().strftime("%d %b %Y")
 
-    outputString = ""
+    output_string = ""
     if location is Location.MOULTON:
-        outputString += f"Moulton Union {meal.capitalize()} - {timestamp}:"
+        output_string += f"Moulton Union {meal.capitalize()} - {timestamp}:"
     if location is Location.THORNE:
-        outputString += f"Thorne {meal.capitalize()} - {timestamp}:"
-    outputString += "\n\n"
+        output_string += f"Thorne {meal.capitalize()} - {timestamp}:"
+    output_string += "\n\n"
 
     for category, items in menu.items():
         if any(menu[category]):
-            outputString += f"{category}:\n"
+            output_string += f"{category}:\n"
         for item in items:
-            if item != None:
-                outputString += f"- {item}\n"
+            if item is not None:
+                output_string += f"- {item}\n"
         if any(menu[category]):
-            outputString += "\n"
+            output_string += "\n"
 
-    return outputString
+    return output_string
 
 
-def sendMessage(text):
+def send_message(text):
+    """
+    POSTs a message to the GroupMe bot.
+    
+    Parameters:
+    - text: The message to be sent.
+    """
     data = {"text": text, "bot_id": botID}
 
     headers = {"Content-Type": "application/json"}
 
-    response = requests.post(groupmeAPI, data=json.dumps(data), headers=headers)
+    response = requests.post(GROUPME_API, data=json.dumps(data), headers=headers, timeout=10)
+    return response
 
 
 if __name__ == "__main__":
     thorne = request(Location.THORNE)
-    thorneMenu = parseResponse(thorne)
+    thorneMenu = parse_response(thorne)
 
     moulton = request(Location.MOULTON)
-    moultonMenu = parseResponse(moulton)
+    moultonMenu = parse_response(moulton)
 
     thorneText = stringify(Location.THORNE, thorneMenu)
     if len(thorneText) < 1000:
-        sendMessage(thorneText)
+        send_message(thorneText)
 
     moultonText = stringify(Location.MOULTON, moultonMenu)
     if len(moultonText) < 1000:
-        sendMessage(moultonText)
+        send_message(moultonText)
