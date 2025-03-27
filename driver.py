45d6087da78f566954596d381780613538eb33d0
Mason Daugherty <mdaugher@bowdoin.edu>
2025-03-26 21:09:09 -0400

    Wicked refactor

--- driver.py
+++ driver.py
@@ -1,20 +1,22 @@
 """
-Module to retrieve and send the current menu for Moulton Union and Thorne Hall.
+Module to retrieve and send the current menu for Moulton Union and
+Thorne Hall.
 """
 
-import os
 import datetime
 import json
+import logging
+import os
 import re
 import xml.etree.ElementTree as ET
-from dotenv import load_dotenv
+
 import requests
-import logging
+from dotenv import load_dotenv
 from tenacity import (
     retry,
-    wait_exponential,
-    stop_after_attempt,
     retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
 )
 
 # ----------------------------------------------------------------------
@@ -25,19 +27,25 @@ logging.basicConfig(
     filename="/home/wbor/bowdoin-menus/bowdoin_menus.log",  # for production
     filemode="a",  # or "w" to overwrite each run
     level=logging.DEBUG,
-    format="%(asctime)s [%(levelname)s] %(message)s",
+    format="%(asctime)s [%(levelname)s] %(funcName)s:%(lineno)d: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 # ----------------------------------------------------------------------
 
 load_dotenv()
 
-botID = os.getenv("BOT_ID")
-if not botID:
+BOT_ID = os.getenv("BOT_ID")
+if not BOT_ID:
     raise ValueError("BOT_ID environment variable is missing or empty!")
 
-MENU_API = "https://apps.bowdoin.edu/orestes/api.jsp"
-GROUPME_API = "https://api.groupme.com/v3/bots/post"
+SPINITRON_PROXY_BASE = os.getenv("SPINITRON_PROXY_BASE", "https://api-1.wbor.org/api")
+if not SPINITRON_PROXY_BASE:
+    RETURN_SONG = False
+else:
+    RETURN_SONG = True
+
+MENU_API = os.getenv("MENU_API", "https://apps.bowdoin.edu/orestes/api.jsp")
+GROUPME_API = os.getenv("GROUPME_API", "https://api.groupme.com/v3/bots/post")
 
 # ----------------------------------------------------------------------
 # CLOSED-STATE TRACKING
@@ -88,14 +96,14 @@ def set_closed_message_sent():
     Creates a file to indicate that we have sent the 'closed' message.
     """
     logging.info("Setting closed-state file to mark 'closed' message as sent.")
-    with open(CLOSED_STATE_FILE, "w") as f:
+    with open(CLOSED_STATE_FILE, "w", encoding="utf-8") as f:
         f.write("CLOSED")
 
 
 def clear_closed_message_state():
     """
-    Removes the file if it exists, signifying that we can
-    send the closed message again in the future if needed.
+    Removes the file if it exists, signifying that we can send the
+    closed message again in the future if needed.
     """
     if os.path.isfile(CLOSED_STATE_FILE):
         logging.info("Removing closed-state file to allow future 'closed' messages.")
@@ -107,7 +115,7 @@ def clear_closed_message_state():
 
 class Location:
     """
-    Represents the location of the menu.
+    Menu ID for a physical location.
     """
 
     MOULTON = 48
@@ -116,7 +124,7 @@ class Location:
 
 class Meals:
     """
-    Represents the meal period.
+    Meal periods.
     """
 
     BREAKFAST = "breakfast"
@@ -124,7 +132,7 @@ class Meals:
     LUNCH = "lunch"
     DINNER = "dinner"
 
-    def get_upcoming_meal(self, location):
+    def get_upcoming_meal(self, location: Location):
         """
         The next upcoming meal is set after the current meal expires.
         During a meal period, it is still 'upcoming'.
@@ -133,7 +141,10 @@ class Meals:
         current_hour = datetime.datetime.now().time().hour
         current_day = datetime.datetime.now().strftime("%a").lower()
         logging.debug(
-            f"Determining upcoming meal for location={location}, day={current_day}, hour={current_hour}."
+            "Determining upcoming meal for location=`%s`, day=`%s`, hour=`%s`.",
+            location,
+            current_day,
+            current_hour,
         )
 
         if location == Location.MOULTON:
@@ -198,7 +209,7 @@ class Meals:
         return Meals.BREAKFAST
 
 
-def build_request(location):
+def build_request(location: Location) -> dict:
     """
     Builds the request data to be sent to the menu API.
     """
@@ -210,42 +221,48 @@ def build_request(location):
         "meal": {meal},
     }
     logging.info(
-        f"Building menu request for location={location}, date={current_date}, meal={meal}."
+        "Building menu request for location=`%s`, date=`%s`, meal=`%s`.",
+        location,
+        current_date,
+        meal,
     )
     return request_data
 
 
-def request(location):
+def request(location: Location) -> str:
     """
     Makes a POST request to the menu API with retry logic.
     """
     data = build_request(location)
-    logging.info(f"Sending POST request to the menu API for location={location}.")
+    logging.info("Sending POST request to the menu API for location=`%s`.", location)
 
     try:
         response = make_post_request(MENU_API, data)
         if response.status_code == 200:
             logging.debug("Received a 200 OK from menu API.")
             return response.content
-        logging.error("Error calling menu API: %s", response.status_code)
+        logging.error("Error calling menu API: `%s`", response.status_code)
     except requests.exceptions.RequestException as e:
-        logging.error("Failed to retrieve menu data: %s", e)
+        logging.error("Failed to retrieve menu data: `%s`", e)
 
     return None
 
 
-def parse_response(request_content):
+def parse_response(request_content: str) -> dict:
     """
     Parses the XML response from the menu API and
-    returns a dictionary like { 'Main Course': [...], 'Desserts': [...], ... }.
-    If there's no data or an error is returned, we return None to indicate no menu.
+    returns a dictionary like:
+    { 'Main Course': [...], 'Desserts': [...], ... }.
+
+    If there's no data or an error is returned, we return None to
+    indicate no menu.
     """
     logging.debug("Parsing XML response from the menu API.")
 
     try:
         root = ET.fromstring(request_content)
     except ET.ParseError as e:
-        logging.error("Failed to parse XML response: %s", e)
+        logging.error("Failed to parse XML response: `%s`", e)
         return None
 
     # Check if the response contains an <error> node with "No records found"
@@ -296,17 +313,19 @@ def parse_response(request_content):
     return sorted_menu
 
 
-def stringify(location, menu):
+def stringify(location: Location, menu: dict) -> str:
     """
-    Converts the menu dictionary into a formatted string.
-    If there's no menu (None or empty), returns an empty string.
+    Converts the menu dictionary into a formatted string. If there's no
+    menu (None or empty), returns an empty string.
     """
     if menu is None:
-        logging.debug(f"No menu data for location={location}. Returning empty string.")
+        logging.debug(
+            "No menu data for location=`%s`. Returning empty string.", location
+        )
         return ""
 
     if not any(menu.values()):
-        logging.debug(f"Menu dictionary is empty for location={location}.")
+        logging.debug("Menu dictionary is empty for location=`%s`.", location)
         return ""
 
     meal = Meals().get_upcoming_meal(location)
@@ -325,111 +344,215 @@ def stringify(location, menu):
     return output_string
 
 
-def send_message(text):
+def send_message(message_text: str) -> requests.Response:
     """
     Sends a message to GroupMe via POST with retry logic.
     """
     logging.info("Sending message to GroupMe bot.")
-    data = {"text": text, "bot_id": botID}
+    data = {"text": message_text, "bot_id": BOT_ID}
     headers = {"Content-Type": "application/json"}
     try:
         response = make_post_request(GROUPME_API, json.dumps(data), headers=headers)
         if response.status_code != 202:
-            logging.warning(f"GroupMe API responded with status {response.status_code}")
+            logging.warning(
+                "GroupMe API responded with status `%s`", response.status_code
+            )
         return response
     except requests.exceptions.RequestException as e:
-        logging.error("Error sending message to GroupMe: %s", e)
+        logging.error("Error sending message to GroupMe: `%s`", e)
         return None
 
 
-def get_now_playing():
+def get_current_spin_details() -> dict:
     """
-    Using WBOR's API, gets the currently playing song with retry logic.
-
-    https://api-1.wbor.org/spins/get
+    Get the most recent spin from a Spinitron API proxy (WBOR's) with
+    retry logic.
     """
-    logging.info("Retrieving currently playing song from WBOR API.")
+    url = SPINITRON_PROXY_BASE + "/spins"
+    logging.info("Retrieving currently playing song from `%s`", url)
     try:
-        response = make_get_request("https://azura.wbor.org/api/station/2/nowplaying")
+        response = make_get_request(url)
         if response.status_code == 200:
             data = response.json()
-            if "now_playing" in data and "song" in data["now_playing"]:
-                song = data["now_playing"]["song"]["title"]
-                artist = data["now_playing"]["song"]["artist"]
-                elapsed = data["now_playing"]["elapsed"]
-                return {"song": song, "artist": artist, "elapsed": elapsed}
-        logging.error("Error calling WBOR API: %s", response.status_code)
+            spins = data.get("items", [])
+            logging.debug("Received %d spins", len(spins))
+            if not spins:
+                logging.info("No spins found in response")
+                return None
+
+            current_spin = spins[0]
+            song = current_spin.get("song")
+            artist = current_spin.get("artist")
+            if not current_spin or not song or not artist:
+                logging.warning("Data missing from Spinitron response!")
+                return None
+
+            duration_s = current_spin.get("duration", 0)
+            start = current_spin.get("start", 0)
+            try:
+                start_time = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S%z")
+            except ValueError as e:
+                logging.error("Error parsing start time `%s`: `%s`", start, e)
+                return None
+
+            elapsed_s = int(
+                (
+                    datetime.datetime.now(datetime.timezone.utc) - start_time
+                ).total_seconds()
+            )
+            logging.debug(
+                "Current spin - song: `%s`, artist: `%s`, duration: `%s`, elapsed: `%s`",
+                song,
+                artist,
+                duration_s,
+                elapsed_s,
+            )
+            return {
+                "song": song,
+                "artist": artist,
+                "duration": duration_s,
+                "elapsed": elapsed_s,
+            }
+        logging.error("Error calling WBOR API: `%s`", response.status_code)
     except requests.exceptions.RequestException as e:
-        logging.error("Error calling WBOR API: %s", e)
+        logging.error("Error calling WBOR API: `%s`", e)
+    return None
+
+
+def get_persona_name(p_id: int) -> str:
+    """
+    Get the persona name from an ID using a Spinitron API proxy (WBOR's) with retry logic.
+    """
+    url = SPINITRON_PROXY_BASE + f"/personas/{p_id}"
+    logging.info("Retrieving persona name from `%s`", url)
+    try:
+        response = make_get_request(url)
+        if response.status_code == 200:
+            data = response.json()
+            name = data.get("name")
+            if not name:
+                logging.warning("Data missing from Spinitron response!")
+                return None
+            logging.debug("Retrieved persona name: `%s`", name)
+            return name
+        logging.error("Error calling WBOR API: `%s`", response.status_code)
+    except requests.exceptions.RequestException as e:
+        logging.error("Error calling WBOR API: `%s`", e)
+    return None
+
+
+def get_current_playlist_details() -> dict:
+    """
+    Get the most recent playlist from a Spinitron API proxy (WBOR's) with retry logic.
+    """
+    url = SPINITRON_PROXY_BASE + "/playlists"
+    logging.info("Retrieving currently playing playlist from `%s`", url)
+    try:
+        response = make_get_request(url)
+        if response.status_code == 200:
+            data = response.json()
+            playlists = data.get("items", [])
+            logging.debug("Received %d playlists", len(playlists))
+            if not playlists:
+                logging.info("No playlists found in response")
+                return None
+
+            current_playlist = playlists[0]
+            title = current_playlist.get("title")
+            p_id = current_playlist.get("persona_id")
+            is_automated = current_playlist.get("automation")
+            if not current_playlist or not title or is_automated is None:
+                logging.warning("Data missing from Spinitron response!")
+                logging.debug(
+                    "Current playlist - title: `%s`, persona_id: `%s`, automation: `%s`",
+                    title,
+                    p_id,
+                    is_automated,
+                )
+                return None
+            logging.debug(
+                "Current playlist - title: `%s`, persona_id: `%s`, automation: `%s`",
+                title,
+                p_id,
+                is_automated,
+            )
+            return {
+                "title": title,
+                "persona_id": p_id,
+                "automation": is_automated,
+            }
+        logging.error("Error calling WBOR API: `%s`", response.status_code)
+    except requests.exceptions.RequestException as e:
+        logging.error("Error calling WBOR API: `%s`", e)
     return None
 
 
 if __name__ == "__main__":
-    logging.info("Starting the menu retrieval script.")
+    logging.info("Starting the menu retrieval script...")
 
-    # 1. Request the data for both halls
     thorne_xml = request(Location.THORNE)
     moulton_xml = request(Location.MOULTON)
-
-    # 2. Parse both
     thorne_menu = parse_response(thorne_xml) if thorne_xml else None
     moulton_menu = parse_response(moulton_xml) if moulton_xml else None
-
-    # 3. Convert both to text
     thorne_text = stringify(Location.THORNE, thorne_menu)
     moulton_text = stringify(Location.MOULTON, moulton_menu)
 
-    # 4. Check if both are empty => "closed" logic
+    # Check if both are empty => closed logic
     if not thorne_text and not moulton_text:
         if not has_closed_message_already_been_sent():
-            logging.info("Both dining halls appear closed, sending closed message.")
-            send_message("The campus dining halls are closed.")
+            logging.info("Both dining halls appear closed, sending closed message...")
+            send_message("The campus dining halls seem to be closed.")
             set_closed_message_sent()
         else:
             logging.info(
-                "Both dining halls still appear closed, but we've already sent the message."
+                "Both dining halls still appear closed, but closed message has already been sent."
             )
     else:
-        logging.info("At least one dining hall has data => clearing closed state.")
+        logging.info("At least one dining hall has data; clearing closed state...")
         clear_closed_message_state()
 
-        now_playing = get_now_playing()
-        expired = now_playing and now_playing["elapsed"] > 900
+        now_playing = get_current_spin_details()
+        playlist = get_current_playlist_details()
+        CURRENTLY_AUTOMATED = bool(playlist.get("automation")) if playlist else None
+        logging.debug("Currently playing automation: `%s`", CURRENTLY_AUTOMATED)
+        PERSONA_ID = playlist.get("persona_id") if playlist else None
+        PERSONA_NAME = get_persona_name(PERSONA_ID) if PERSONA_ID else None
 
-        if thorne_text and moulton_text:
-            logging.info("Both dining halls have menu data.")
-            logging.info("Append now playing song info to Moulton's menu.")
-            if now_playing and not expired:
-                moulton_text += f"Now playing on WBOR.org: {now_playing['song']} by {now_playing['artist']}"
-        else:
-            logging.info("Only one dining hall has menu data.")
-            logging.info(
-                "Add the now playing song info to the menu of the hall that has data."
+        CURRENTLY_AUTOMATED = False  # TESTING
+        if playlist and CURRENTLY_AUTOMATED:
+            logging.debug("Automation playlist detected; skipping song info.")
+            SONG_INFO = ""
+        elif now_playing and now_playing["elapsed"] <= 900:
+            SONG_INFO = (
+                f"Now playing on WBOR(.org): {now_playing.get("artist", "")} -"
+                f" {now_playing["song"]} on {playlist["title"]} with {PERSONA_NAME}"
             )
-            if now_playing and not expired:
+            logging.debug("Song info: `%s`", SONG_INFO)
+        else:
+            logging.debug("Some other condition; skipping song info.")
+            SONG_INFO = ""
+
+        if SONG_INFO:
+            logging.debug("Appending song info to menu text...")
+            if thorne_text and moulton_text:
+                logging.info(
+                    "Both dining halls have menu data; appending song info to Moulton's menu."
+                )
+                moulton_text += SONG_INFO
+            else:
+                logging.info("Only one dining hall has menu data; appending song info.")
                 if thorne_text:
-                    thorne_text += f"Now playing on WBOR.org: {now_playing['song']} by {now_playing['artist']}"
+                    thorne_text += SONG_INFO
                 if moulton_text:
-                    moulton_text += f"Now playing on WBOR.org: {now_playing['song']} by {now_playing['artist']}"
+                    moulton_text += SONG_INFO
 
-        # If Thorne has menu text, send it
-        if thorne_text:
-            if len(thorne_text) < 1000:
-                send_message(thorne_text)
-            else:
-                logging.warning(
-                    "Thorne text is too long to send (>1000 chars). Printing locally."
-                )
-                print(thorne_text)
-
-        # If Moulton has menu text, send it
-        if moulton_text:
-            if len(moulton_text) < 1000:
-                send_message(moulton_text)
-            else:
-                logging.warning(
-                    "Moulton text is too long to send (>1000 chars). Printing locally."
-                )
-                print(moulton_text)
-
-    logging.info("Menu retrieval script finished.")
+        for text, label in ((thorne_text, "Thorne"), (moulton_text, "Moulton")):
+            if text:
+                if len(text) < 1000:
+                    send_message(text)
+                else:
+                    logging.critical(
+                        "`%s` text is too long to send (>1000 chars). Printing locally...",
+                        label,
+                    )
+                    print(text)
